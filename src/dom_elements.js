d8f28815f76f1e4bf579f2a80e7c164cc5a4315b
parthiv-2006 <parthiv.paul5545@gmail.com>
2025-07-21 17:48:35 -0400

    Add references to form elements

--- src/dom_elements.js
+++ src/dom_elements.js
@@ -3,6 +3,13 @@ export default class DomElements {
         this.createProject = document.querySelector('#createProject');
         this.createTask = document.querySelector('#createTask');
         this.headerTitle = document.querySelector('#title')
+        this.taskForm = document.querySelector('#task-form')
+        this.taskName = document.querySelector('#task-name')
+        this.taskDescription = document.querySelector('#task-description')
+        this.taskDate = document.querySelector('#task-date')
+        this.taskPriority = document.querySelector('#task-priority')
+        this.addTaskButton = document.querySelector('#add-task-button')
+        this.mainContent = document.querySelector('.main')
 
     }
 }
