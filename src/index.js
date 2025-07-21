429a701d8d726fdae6935a0da687dc15853a1365
parthiv-2006 <parthiv.paul5545@gmail.com>
2025-07-21 17:48:03 -0400

    Add a function to create task card in the main section

--- src/index.js
+++ src/index.js
@@ -8,7 +8,36 @@ dom.createTask.addEventListener('click', () => {
     dom.taskForm.classList.remove('hidden');
 })
 
+dom.taskForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // Still need this to prevent the page from reloading
+    createTaskCard();
+    dom.taskForm.reset(); 
+    dom.taskForm.classList.add('hidden');
+})
 
-function CreateTaskCard() {
+// Function names in JavaScript are typically camelCase.
+function createTaskCard() {
+    const card = document.createElement('div');
+    card.classList.add('task-card');
     
+    const name = document.createElement('p');
+    name.textContent = `Task Name: ${dom.taskName.value}`;
+    card.appendChild(name);
+
+    if (dom.taskDescription.value.trim() !== ''){
+        const description = document.createElement('p');
+        description.textContent = `Description: ${dom.taskDescription.value}`;
+        card.appendChild(description);}
+
+    const date = document.createElement('p');
+    if (dom.taskDate.value.trim() !== ''){
+        date.textContent = `Due Date: ${dom.taskDate.value}`;
+        card.appendChild(date);}
+
+    if (dom.taskPriority.value.trim() !== 'none') {
+        const priority = document.createElement('p');
+        priority.textContent = `Priority: ${dom.taskPriority.value}`;
+        card.appendChild(priority);}
+
+    dom.mainContent.appendChild(card);
 }
