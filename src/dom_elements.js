044c2c5f71a73def467618d2c99ea44d247ce01f
parthiv-2006 <parthiv.paul5545@gmail.com>
2025-07-21 14:17:30 -0400

    Add Module for Dom Elements

---
+++ src/dom_elements.js
@@ -0,0 +1,8 @@
+export default class DomElements {
+    constructor () {
+        this.createProject = document.querySelector('#createProject');
+        this.createTask = document.querySelector('#createTask');
+        this.headerTitle = document.querySelector('#title')
+
+    }
+}
